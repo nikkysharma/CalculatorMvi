0dbbc1bea1d2856ece1c91f5cbf905ff06f6358b
abhinav_sharma <nikkysharma654@gmail.com>
2024-04-08 15:11:40 +0530

    new cahnges

--- app/src/main/java/com/example/myapplication/CalculatorUtil.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication
-
-object CalculatorUtil {
-    fun Double.getCalValue(secondValue: Double, lamba: (first:Double, secondValue: Double) -> Double) :Double {
-        return Math.round(this.getCalValue(secondValue,lamba) * 10.0) / 10.0
-    }
-}
